6fa0f76200f794f7054b2253e07b5960fbe066ac
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-14 13:28:33 +0300

    Show how to reverse the integer method 2

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,8 +10,11 @@
 
 function reverseInt(n) {
   const reversed = n.toString().split('').reverse().join('')
-  const sign = Math.sign(n)
-  return parseInt(reversed) * sign
+  if (n < 0) {
+    return parseInt(reversed) * -1
+  } else {
+    return parseInt(reversed) * 1
+  }
 }
 
 module.exports = reverseInt;
