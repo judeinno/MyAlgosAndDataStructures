0ac90b78c3cf6e4ef475813325c4de0db58d4b09
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-08-14 11:19:18 +0300

    Add reversed solution 3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,11 +7,9 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = "";
-  for (let character of str) {
-    reversed = character + reversed;
-  }
-  return reversed
+  return str.split('').reduce((rev, char) => {
+    return char + rev;
+  }, '')
 }
 
 module.exports = reverse;
