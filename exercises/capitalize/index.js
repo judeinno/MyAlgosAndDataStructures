e04b1f12959cf7e6646c1cf27c0bed318778b574
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-12 14:38:04 +0300

    Capitalise solved part 1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = []
+  const splitSentence = str.split(" ");
+  for (let word of splitSentence) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(" ");
+}
 
 module.exports = capitalize;
