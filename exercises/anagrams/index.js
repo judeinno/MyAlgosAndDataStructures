4f1f18c78a03dff99fbd98c5c10792486a57492a
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-12 14:13:02 +0300

    Anagram solved part 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+  const aCharMapKeys = Object.keys(aCharMap).length
+  const bCharMapKeys = Object.keys(bCharMap).length
+  if (aCharMapKeys !== bCharMapKeys) {
+    return false;
+  }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {}
+  const cleanedStr = str.replace(/[^\w]/g, "").toLowerCase();
+  for (let char of cleanedStr) {
+    charMap[char] = char[char] + 1 || 1
+  }
+  return charMap
+}
 
 module.exports = anagrams;
