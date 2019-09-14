a7328543b8238cf115c0cdbacdb0fdcb6f8bea1b
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-14 09:27:17 +0300

    Add a linear search function

---
+++ exercises/MasterClassAlgos/searchAlgorithms/linearSeach.js
@@ -0,0 +1,12 @@
+function linearSearch(arr, val){
+  for(var i = 0; i < arr.length; i++){
+      if(arr[i] === val) return i;
+  }
+  return -1;
+}
+
+linearSearch([34,51,1,2,3,45,56,687], 100)
+
+// BestCase O(1)
+// AverageCase O(n)
+// WorstCase O(n)
